05c452adee8c71485ccf8e6b5d0709d540651b9e
Marco Ciampa <ciampix@libero.it>
2014-11-12 09:37:25 +0100

    Updated pdf generation

--- doc/runme.sh
+++ doc/runme.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 DOC=kicad-doc-doc
+DBLATEXOPTS="-P latex.output.revhistory=0 -P doc.publisher.show=0"
 
 echo "Converting $DOC.adoc to html..."
 asciidoc $DOC.adoc
 
 echo "Converting $DOC.adoc to pdf..."
-a2x -f pdf  $DOC.adoc
+a2x -f pdf --dblatex-opts "$DBLATEXOPTS" $DOC.adoc
 
 echo "Converting $DOC.adoc to epub..."
 a2x -f epub $DOC.adoc
