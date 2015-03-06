546036d7a7f74f4c229629ea211633d238ea4c0e
Marco Ciampa <ciampix@libero.it>
2015-03-06 11:45:24 +0100

    Add side toc for html content

--- utils/docker/make.docker.sh
+++ utils/docker/make.docker.sh
@@ -8,14 +8,19 @@ adocRoot=/mnt/src/kicad-doc/src/asciidoc
 # function build each sub directory
 function makeAll {
         cd "$adocRoot/$1"
+            export TOC2='-a toc2' #Set side toc for HTML
             ./make.sh all-all
 }
 
 cd "$adocRoot"
 
 # build each sub directory
-makeAll KiCad
-makeAll Eeschema
-makeAll IDF_Exporter
 makeAll CvPcb
+makeAll Eeschema
+makeAll GerbView
 makeAll Getting_Started_in_KiCad
+makeAll IDF_Exporter
+makeAll Interactive_Router
+makeAll KiCad
+makeAll Pcbnew
+makeAll Pl_Editor
