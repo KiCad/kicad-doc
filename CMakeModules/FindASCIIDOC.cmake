378e05268bc8c6ef5a36fe14864f50ae1e0e257f
Brian Sidebotham <brian.sidebotham@gmail.com>
2015-04-10 21:13:27 +0100

    Installed Asciidoctor and PO4A take precedence over "included" copies

--- CMakeModules/FindASCIIDOC.cmake
+++ CMakeModules/FindASCIIDOC.cmake
@@ -9,33 +9,29 @@ unset( ASCIIDOC_FOUND )
 unset( _DEBUG )
 set( _DBG OFF )
 
-# Attempt to execute the asciidoc utility
-set( ASCIIDOC_COMMAND "asciidoc" )
-execute_process( COMMAND ${ASCIIDOC_COMMAND} --version
-                 RESULT_VARIABLE _ADOC_EXE_RESULT
-                 OUTPUT_VARIABLE _ADOC_EXE_OUTPUT
-                 ERROR_VARIABLE _ADOC_EXE_ERROR )
+# Try to use CMake's standard way of finding programs...
+find_program( ASCIIDOC_COMMAND asciidoc )
 
-if( _DBG )
-    message( STATUS "asciidoc result: ${_ADOC_EXE_RESULT}" )
-    message( STATUS "asciidoc outut: ${_ADOC_EXE_OUTPUT}" )
-endif()
+if( NOT ASCIIDOC_COMMAND )
 
-if( NOT "${_ADOC_EXE_RESULT}" STREQUAL "0" )
-    # Attempt to see if a bat file can be run which invokes asciidoc
-    # A lot of windows users need this "bodge" in order to allow asciidoc
-    # to be run with the asciidoc command as python doesn't usually
-    # register .py as an executable file type
-    set( ASCIIDOC_COMMAND "asciidoc.bat" )
-
+    # Attempt to execute the asciidoc utility
+    set( ASCIIDOC_COMMAND "asciidoc" )
     execute_process( COMMAND ${ASCIIDOC_COMMAND} --version
-            RESULT_VARIABLE _ADOC_EXE_RESULT
-            OUTPUT_VARIABLE _ADOC_EXE_OUTPUT
-            ERROR_VARIABLE _ADOC_EXE_ERROR )
+                     RESULT_VARIABLE _ADOC_EXE_RESULT
+                     OUTPUT_VARIABLE _ADOC_EXE_OUTPUT
+                     ERROR_VARIABLE _ADOC_EXE_ERROR )
+
+    if( _DBG )
+        message( STATUS "asciidoc result: ${_ADOC_EXE_RESULT}" )
+        message( STATUS "asciidoc outut: ${_ADOC_EXE_OUTPUT}" )
+    endif()
 
     if( NOT "${_ADOC_EXE_RESULT}" STREQUAL "0" )
-        # Finally, check we can use the supplied asciidoc version
-        set( ASCIIDOC_COMMAND python ${PROJECT_SOURCE_DIR}/CMakeSupport/asciidoc/asciidoc.py )
+        # Attempt to see if a bat file can be run which invokes asciidoc
+        # A lot of windows users need this "bodge" in order to allow asciidoc
+        # to be run with the asciidoc command as python doesn't usually
+        # register .py as an executable file type
+        set( ASCIIDOC_COMMAND "asciidoc.bat" )
 
         execute_process( COMMAND ${ASCIIDOC_COMMAND} --version
                 RESULT_VARIABLE _ADOC_EXE_RESULT
@@ -43,7 +39,19 @@ if( NOT "${_ADOC_EXE_RESULT}" STREQUAL "0" )
                 ERROR_VARIABLE _ADOC_EXE_ERROR )
 
         if( NOT "${_ADOC_EXE_RESULT}" STREQUAL "0" )
-            set( ASCIIDOC_FOUND FALSE )
+            # Finally, check we can use the supplied asciidoc version
+            set( ASCIIDOC_COMMAND python ${PROJECT_SOURCE_DIR}/CMakeSupport/asciidoc/asciidoc.py )
+
+            execute_process( COMMAND ${ASCIIDOC_COMMAND} --version
+                    RESULT_VARIABLE _ADOC_EXE_RESULT
+                    OUTPUT_VARIABLE _ADOC_EXE_OUTPUT
+                    ERROR_VARIABLE _ADOC_EXE_ERROR )
+
+            if( NOT "${_ADOC_EXE_RESULT}" STREQUAL "0" )
+                set( ASCIIDOC_FOUND FALSE )
+            else()
+                set( ASCIIDOC_FOUND TRUE )
+            endif()
         else()
             set( ASCIIDOC_FOUND TRUE )
         endif()
@@ -55,9 +63,14 @@ else()
 endif()
 
 if( ASCIIDOC_FOUND )
-    # Get the PO4A version number...
+    # Get the ASCIIDOC version number...
+    execute_process( COMMAND ${ASCIIDOC_COMMAND} --version
+            RESULT_VARIABLE _ADOC_EXE_RESULT
+            OUTPUT_VARIABLE _ADOC_EXE_OUTPUT
+            ERROR_VARIABLE _ADOC_EXE_ERROR )
+
     string( REGEX MATCH "[0-9]+\\.[0-9]+\\.[0-9]+" ADOC_VERSION "${_ADOC_EXE_OUTPUT}" )
-    message( STATUS "Asciidoc Found v${ADOC_VERSION}" )
+    message( STATUS "Asciidoc Found ${ASCIIDOC_COMMAND} v${ADOC_VERSION}" )
 else()
     message( STATUS "Asciidoc NOT FOUND" )
 endif()
--- CMakeModules/FindPO4A.cmake
+++ CMakeModules/FindPO4A.cmake
@@ -14,51 +14,53 @@ unset( PO4A_FOUND )
 # Set to ON to get debug information from this module
 set( DBG OFF )
 
-# Set the vanilla command for PO4a until proven otherwise
-set( PO4A_COMMAND po4a )
+find_program( PO4A_COMMAND po4a )
 
-# Attempt to execute the po4a utility
-execute_process( COMMAND ${PO4A_COMMAND} --version
-                 RESULT_VARIABLE _PO4A_EXE_RESULT
-                 OUTPUT_VARIABLE _PO4A_EXE_OUTPUT
-                 ERROR_VARIABLE _PO4A_EXE_ERROR )
+if( NOT PO4A_COMMAND )
 
-if( DBG )
-    message( DEBUG " ${PO4A_COMMAND} ${_PO4A_EXE_RESULT} ${_PO4A_EXE_OUTPUT}" )
-endif()
+    # Set the vanilla command for PO4a until proven otherwise
+    set( PO4A_COMMAND po4a )
 
-if( NOT "${_PO4A_EXE_RESULT}" STREQUAL "0" )
-    # If a system version is not found, attempt to use the version we've included in the
-    # CMakeSupport folder (mainly for Windows users!)
-    set( PO4A_BASE_COMMAND perl -I ${PROJECT_SOURCE_DIR}/CMakeSupport/po4a/lib ${PROJECT_SOURCE_DIR}/CMakeSupport/po4a/ )
-    set( PO4A_COMMAND "${PO4A_BASE_COMMAND}po4a" )
-    set( PO4A_TRANSLATE_COMMAND "${PO4A_BASE_COMMAND}po4a-translate" )
-    set( PO4A_GETTEXTIZE_COMMAND "${PO4A_BASE_COMMAND}po4a-gettextize" )
-
+    # Attempt to execute the po4a utility
     execute_process( COMMAND ${PO4A_COMMAND} --version
                      RESULT_VARIABLE _PO4A_EXE_RESULT
                      OUTPUT_VARIABLE _PO4A_EXE_OUTPUT
                      ERROR_VARIABLE _PO4A_EXE_ERROR )
 
     if( DBG )
-        message( DEBUG " ${PO4A_COMMAND}\n${PO4A_COMMAND_ARGS}\n${_PO4A_EXE_RESULT}\n${_PO4A_EXE_OUTPUT}\n${_PO4A_EXE_ERROR}" )
+        message( DEBUG " ${PO4A_COMMAND} ${_PO4A_EXE_RESULT} ${_PO4A_EXE_OUTPUT}" )
     endif()
 
     if( NOT "${_PO4A_EXE_RESULT}" STREQUAL "0" )
         # If a system version is not found, attempt to use the version we've included in the
         # CMakeSupport folder (mainly for Windows users!)
-        set( PO4A_FOUND FALSE )
+        set( PO4A_BASE_COMMAND perl -I ${PROJECT_SOURCE_DIR}/CMakeSupport/po4a/lib ${PROJECT_SOURCE_DIR}/CMakeSupport/po4a/ )
+        set( PO4A_COMMAND "${PO4A_BASE_COMMAND}po4a" )
+        set( PO4A_TRANSLATE_COMMAND "${PO4A_BASE_COMMAND}po4a-translate" )
+        set( PO4A_GETTEXTIZE_COMMAND "${PO4A_BASE_COMMAND}po4a-gettextize" )
+    
+        execute_process( COMMAND ${PO4A_COMMAND} --version
+                         RESULT_VARIABLE _PO4A_EXE_RESULT
+                         OUTPUT_VARIABLE _PO4A_EXE_OUTPUT
+                         ERROR_VARIABLE _PO4A_EXE_ERROR )
     else()
         set( PO4A_FOUND TRUE )
     endif()
 else()
+    set( PO4A_TRANSLATE_COMMAND "${PO4A_COMMAND}-translate" )
+    set( PO4A_GETTEXTIZE_COMMAND "${PO4A_COMMAND}-gettextize" )
     set( PO4A_FOUND TRUE )
 endif()
 
 if( PO4A_FOUND )
     # Get the PO4A version number...
+    execute_process( COMMAND ${PO4A_COMMAND} --version
+                     RESULT_VARIABLE _PO4A_EXE_RESULT
+                     OUTPUT_VARIABLE _PO4A_EXE_OUTPUT
+                     ERROR_VARIABLE _PO4A_EXE_ERROR )
+
     string( REGEX MATCH "[0-9]+\\.[0-9]+" PO4A_VERSION "${_PO4A_EXE_OUTPUT}" )
-    message( STATUS "PO4A Found v${PO4A_VERSION}" )
+    message( STATUS "PO4A Found ${PO4A_COMMAND} v${PO4A_VERSION}" )
 endif()
 
 macro( po4a ARGS )
